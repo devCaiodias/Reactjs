455c080d8e53c48def2011b119bb57a013e98555
devCaiodias <caiodiasmartins12@gmail.com>
2024-09-14 16:19:18 -0300

    Avançando em props

--- aula-react/src/components/Item.js
+++ aula-react/src/components/Item.js
@@ -1,9 +1,21 @@
-function Item(props) {
+import PropTypes from 'prop-types'
+
+function Item({marca, lancamento}) {
     return (
         <>
-            <li>{props.marca}</li>
+            <li>{marca} - {lancamento}</li>
         </>
     )
+}
+
+Item.prototype = {
+    marca: PropTypes.string.isRequired,
+    lancamento: PropTypes.number.isRequired,
+}
+
+Item.defaultProps = {
+    marca: "havai",
+    lancamento: 2556
 }
 
 export default Item;
--- aula-react/src/components/List.js
+++ aula-react/src/components/List.js
@@ -10,7 +10,12 @@ function List() {
                 <li>Item 1</li>
                 <li>Item 2</li>
                 <li>Item 3</li>
-                <Item marca={marca}/>
+                <Item marca={marca} lancamento={2006} />
+                <Item marca='Adidas' lancamento={2005} />
+                <Item marca='pai' lancamento={2002} />
+                <Item marca='lloo' lancamento={2889} />
+                <Item marca='piracanjuba' lancamento={255} />
+                <Item />
             </ul>
         </>
     )
